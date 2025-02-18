744ff918940b909ec01753244d671bf3ddc58763
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-18 13:52:16 +0530

    Add skeleton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,23 @@
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    contentPath(import.meta.url, 'svelte')
+  ],
 
   theme: {
     extend: {}
   },
 
-  plugins: [typography]
+  plugins: [
+    typography,
+    skeleton({
+      themes: [ themes.cerberus, themes.rose ]
+    })
+  ]
 } satisfies Config;
