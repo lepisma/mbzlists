567651082fc93b3e856bc031b54cce5929e2624c
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-25 15:06:04 +0530

    Add metadata APIs

---
+++ src/routes/api/metadata/edit/[editid]/+server.ts
@@ -0,0 +1,21 @@
+import db from '$lib/server/db';
+import { json } from '@sveltejs/kit';
+
+export async function GET({ params }) {
+  const res: any = db.prepare('SELECT id, edit_id, name, created_on, last_modified_on, description, cover_art, is_public FROM lists WHERE edit_id = ?').get(params.editid);
+
+  if (res) {
+    return json({
+      viewId: res.id,
+      editId: res.edit_id,
+      name: res.name,
+      createdOn: res.created_on,
+      lastModifiedOn: res.last_modified_on,
+      description: res.description,
+      coverArt: res.cover_art,
+      isPublic: res.is_public === 1,
+    });
+  } else {
+    return json({ success: false, error: 'Not found' },  { status: 404 });
+  }
+}
---
+++ src/routes/api/metadata/list/[listid]/+server.ts
@@ -0,0 +1,20 @@
+import db from '$lib/server/db';
+import { json } from '@sveltejs/kit';
+
+export async function GET({ params }) {
+  const res: any = db.prepare('SELECT id, name, created_on, last_modified_on, description, cover_art, is_public FROM lists WHERE id = ?').get(params.listid);
+
+  if (res) {
+    return json({
+      viewId: res.id,
+      name: res.name,
+      createdOn: res.created_on,
+      lastModifiedOn: res.last_modified_on,
+      description: res.description,
+      coverArt: res.cover_art,
+      isPublic: res.is_public === 1,
+    });
+  } else {
+    return json({ success: false, error: 'Not found' },  { status: 404 });
+  }
+}
