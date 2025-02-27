5585d4a74c0e934f96b63deed5b190a176df4716
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-27 13:19:10 +0530

    Show visibility status in list card

--- src/lib/components/ListCard.svelte
+++ src/lib/components/ListCard.svelte
@@ -1,5 +1,7 @@
 <script lang='ts'>
   import IconTrash from 'virtual:icons/la/trash';
+  import IconLock from 'virtual:icons/la/lock';
+  import IconGlobe from 'virtual:icons/la/globe';
   import { formatDistanceToNow } from 'date-fns';
 
   let { list, deleteCallback = null } = $props();
@@ -7,7 +9,16 @@
 
 <div class="flex items-center justify-between p-3 rounded-lg border">
   <div>
-    <div class="font-medium"><a href={list.editId ? `/edit/${list.editId}` : `/list/${list.viewId}`}>{list.name}</a></div>
+    <div class="font-medium">
+      <a class="flex items-center" href={list.editId ? `/edit/${list.editId}` : `/list/${list.viewId}`}>
+        {#if list.isPublic }
+          <IconGlobe class="mr-1"/>
+        {:else}
+          <IconLock class="mr-1"/>
+        {/if}
+        {list.name}
+      </a>
+    </div>
     <span class="text-sm text-gray-400" title={list.createdOn}>Created: {formatDistanceToNow(list.createdOn, { addSuffix: true })}, </span>
     <span class="text-sm text-gray-400" title={list.lastModifiedOn}>Modified: {formatDistanceToNow(list.lastModifiedOn, { addSuffix: true })}</span>
   </div>
