7fae63ae75bc085509a602b9266e41366a51b8a8
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-27 11:07:08 +0530

    Memoize yt-search

--- src/lib/server/resolution.ts
+++ src/lib/server/resolution.ts
@@ -1,8 +1,11 @@
 // Basic content resolution utilities
 
 import yts from 'yt-search';
+import pMemoize from 'p-memoize';
 
-export async function ytFirstVideoId(search: string): Promise<string | null> {
+async function ytFirstVideoIdInt(search: string): Promise<string | null> {
   let r = await yts(search);
   return r.videos ? r.videos[0].videoId : null;
 }
+
+export const ytFirstVideoId = pMemoize(ytFirstVideoIdInt);
