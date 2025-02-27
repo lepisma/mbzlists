18b0a66ef0dbb0c1e636f04f8187c417e7032e17
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-27 12:05:25 +0530

    Add playlist playback support for youtube

--- src/routes/edit/[editid]/+page.svelte
+++ src/routes/edit/[editid]/+page.svelte
@@ -8,7 +8,7 @@
   import { loadEditableList, createList, saveList } from '$lib/ops';
   import { getCoverArt, queryMB } from '$lib/mb';
   import type { EditableList, Song } from '$lib/types';
-  import type { playTrackOnSpotify, playTrackOnYt } from '$lib/playback';
+  import { playTrackOnSpotify, playTrackOnYt, playListOnYt } from '$lib/playback';
   import { rememberItem } from '$lib/utils';
   import { formatDistanceToNow } from 'date-fns';
   import IconTrash from 'virtual:icons/la/trash';
@@ -132,8 +132,7 @@
         {#if playDropdownState }
           <div class="absolute z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white ring-1 shadow-lg ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
-              <!-- Active: "bg-gray-100 text-gray-900 outline-hidden", Not Active: "text-gray-700" -->
-              <a href="#" class="flex hover:bg-gray-100 items-center px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0"><IconYoutubeIcon class="mr-2" /> Play on Youtube</a>
+              <a href="#" onclick={async () => await playListOnYt(list)} class="flex hover:bg-gray-100 items-center px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0"><IconYoutubeIcon class="mr-2" /> Play on Youtube</a>
             </div>
             <div class="py-1" role="none">
               <a href={`/api/list/${list.viewId}?type=xspf`} class="flex hover:bg-gray-100 items-center px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2"><IconDownload class="mr-2" /> Download as XSPF</a>
--- src/routes/list/[listid]/+page.svelte
+++ src/routes/list/[listid]/+page.svelte
@@ -8,7 +8,7 @@
   import { getCoverArt } from '$lib/mb';
   import { rememberItem } from '$lib/utils';
   import type { List } from '$lib/types';
-  import type { playTrackOnSpotify, playTrackOnYt } from '$lib/playback';
+  import { playTrackOnSpotify, playTrackOnYt, playListOnYt } from '$lib/playback';
   import { goto } from '$app/navigation';
   import { formatDistanceToNow } from 'date-fns';
   import IconDownload from 'virtual:icons/la/download';
@@ -66,8 +66,7 @@
         {#if playDropdownState }
           <div class="absolute z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white ring-1 shadow-lg ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
-              <!-- Active: "bg-gray-100 text-gray-900 outline-hidden", Not Active: "text-gray-700" -->
-              <a href="#" class="flex hover:bg-gray-100 items-center px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0"><IconYoutubeIcon class="mr-2" /> Play on Youtube</a>
+              <a href="#" onclick={async () => await playListOnYt(list)} class="flex hover:bg-gray-100 items-center px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0"><IconYoutubeIcon class="mr-2" /> Play on Youtube</a>
             </div>
             <div class="py-1" role="none">
               <a href={`/api/list/${list.viewId}?type=xspf`} class="flex hover:bg-gray-100 items-center px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2"><IconDownload class="mr-2" /> Download as XSPF</a>
