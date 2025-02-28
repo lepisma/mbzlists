27241fc5ba8c3e644dfaf9107481927b8292461d
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-28 20:58:35 +0530

    Support random quotes in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,18 @@
 <script>
+  function pickRandomQuote() {
+    let quotes = [
+      '"Hello" - Adele',
+      '"I can see you laughing through dreams of perfect sleep" - Slowdive',
+      '"All secrets sleep in winter clothes" - Neutral Milk Hotel',
+      '"You got a face not spoiled by beauty" - U2',
+    ];
+    return quotes[Math.floor(Math.random() * quotes.length)];
+  }
 </script>
 
 <footer class="p-6 rounded-b-lg">
   <blockquote class="blockquote mb-4">
-    "Hello" - Adele
+    {pickRandomQuote()}
   </blockquote>
   <p class="text-italic">
     <em>mbzlists</em> is an <a class="anchor" href="https://github.com/lepisma/mbzlists">Open Source Software</a> designed and made in Bengaluru by <a class="anchor" href="https://unnu.so">unnu</a> and <a class="anchor" href="https://lepisma.xyz">lepisma</a>
