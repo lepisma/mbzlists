cb01dfb39dbd74821847894cb9a9033b23168c93
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-28 12:51:32 +0530

    Rename play list button to be less confusing

--- src/lib/components/PlayListPlayButton.svelte
+++ src/lib/components/PlayListPlayButton.svelte
@@ -14,7 +14,7 @@
   <div>
     <button type="button" onclick={() => {dropdownState = !dropdownState}} class="btn btn-sm preset-filled-primary-500 inline-flex w-full justify-center gap-x-1.5 px-3 py-2" id="menu-button" aria-expanded="true" aria-haspopup="true">
       <IconPlay/>
-      Play List
+      Play
       <svg class="-mr-1 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
         <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
       </svg>
