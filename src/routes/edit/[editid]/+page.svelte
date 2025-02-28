77986b489e23b5c556d1ffdd57f0d0c120bec02d
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-28 13:18:01 +0530

    Redo edit page view

--- src/routes/edit/[editid]/+page.svelte
+++ src/routes/edit/[editid]/+page.svelte
@@ -2,7 +2,6 @@
   import { page } from '$app/stores';
   import { onMount } from 'svelte';
   import debounce from 'lodash/debounce';
-  import QrCode from '$lib/components/QrCode.svelte';
   import SongDuration from '$lib/components/SongDuration.svelte';
   import PlayListDuration from '$lib/components/PlayListDuration.svelte';
   import PlayListPlayButton from '$lib/components/PlayListPlayButton.svelte';
@@ -15,13 +14,11 @@
   import { formatDistanceToNow } from 'date-fns';
   import IconTrash from 'virtual:icons/la/trash';
   import IconCopy from 'virtual:icons/la/copy';
-  import IconLock from 'virtual:icons/la/lock';
-  import IconGlobe from 'virtual:icons/la/globe';
   import IconYoutubeIcon from 'virtual:icons/logos/youtube-icon';
   import IconSpotify from 'virtual:icons/logos/spotify';
   import { overrideItemIdKeyNameBeforeInitialisingDndZones, dndzone } from 'svelte-dnd-action';
   import { flip } from 'svelte/animate';
-  import { Switch } from '@skeletonlabs/skeleton-svelte';
+  import { OutClick } from 'svelte-outclick';
 
   overrideItemIdKeyNameBeforeInitialisingDndZones('mbid');
 
@@ -85,11 +82,6 @@
     await saveList(list);
   }
 
-  async function handlePublicToggle(e) {
-    list = {...list, isPublic: e.checked, lastModifiedOn: new Date()};
-    await saveList(list);
-  }
-
   onMount(async () => {
     list = await loadEditableList(list.editId);
 
@@ -102,46 +94,35 @@
   <title>{list.name} [editing] | mbzlists</title>
 </svelte:head>
 
-<div class="grid grid-cols-3 gap-6">
-  <div class="col-span-2">
+<div class="mb-7">
+  <div>
     <input
       type="text"
       class="text-4xl font-semibold mb-2 bg-transparent border-0 border-b-2 border-gray-400 focus:ring-0 focus:border-primary-500 transition-colors pl-0"
       bind:value={list.name}
       oninput={handleNameEdits}
       />
-    <span class="text-sm text-gray-400" title={list.createdOn}>Created: {formatDistanceToNow(list.createdOn, { addSuffix: true })}, </span>
-    <span class="text-sm text-gray-400" title={list.lastModifiedOn}>Modified: {formatDistanceToNow(list.lastModifiedOn, { addSuffix: true })}</span>
-    <h3 class="text-l mt-2 font-italic mb-4">Total {list.items.length} songs. Duration <PlayListDuration list={list} />.</h3>
-
-    <div class="flex space-x-4 mb-4">
-      <PlayListPlayButton list={list} />
-      <ShareButton list={list} isEdit={true} />
-      <button onclick={cloneList} class="btn btn-sm preset-filled-primary-500"><IconCopy />Make a Copy</button>
-    </div>
-
-    <div class="flex space-x-3">
-      <span>Make {#if list.isPublic } private {:else} public {/if} </span>
-        <Switch name="public" bind:checked={list.isPublic} onCheckedChange={handlePublicToggle}>
-          {#snippet inactiveChild()}<IconLock />{/snippet}
-          {#snippet activeChild()}<IconGlobe />{/snippet}
-        </Switch>
-    </div>
   </div>
-
-  <div class="flex justify-center items-center">
-    {#if list.viewId }
-      <QrCode viewId={list.viewId} />
-    {/if}
+  <div class="text-sm text-gray-400">
+    <span title={list.createdOn}>Created: {formatDistanceToNow(list.createdOn, { addSuffix: true })}, </span>
+    <span title={list.lastModifiedOn}>modified: {formatDistanceToNow(list.lastModifiedOn, { addSuffix: true })}</span>
   </div>
+  <div class="mt-2 font-italic mb-4">Total {list.items.length} songs, duration: <PlayListDuration list={list} /></div>
 
-  <div class="space-y-2 col-span-full">
-    {#if list.items.length === 0}
-      <p class="italic">Your list is empty. Search for songs to add!</p>
-    {:else}
-      <section use:dndzone={{ items: list.items, flipDurationMs: 200 }} onconsider={handleSort} onfinalize={handleSort}>
+  <div class="flex space-x-2">
+    <PlayListPlayButton list={list} />
+    <ShareButton list={list} isEdit={true} />
+    <button onclick={cloneList} class="btn btn-sm preset-filled-primary-500"><IconCopy />Make a Copy</button>
+  </div>
+</div>
+
+<div>
+  {#if list.items.length === 0}
+    <p class="italic">Your list is empty. Search for songs to add!</p>
+  {:else}
+    <section use:dndzone={{ items: list.items, flipDurationMs: 200 }} onconsider={handleSort} onfinalize={handleSort}>
       {#each list.items as item(item.mbid)}
-        <div class="flex items-center justify-between p-3 rounded-blg border shadow-md" animate:flip={{duration: 200}}>
+        <div class="flex items-center justify-between mt-2 p-3 rounded-lg border shadow-md" animate:flip={{duration: 200}}>
           <div class="flex">
             <div>
               {#await getCoverArt(item.release.mbid)}
@@ -166,37 +147,39 @@
           </div>
           <button
             type="button"
-              onclick={async () => await removeItem(item.mbid)}
-              class="btn preset-filled-error-500"
+            onclick={async () => await removeItem(item.mbid)}
+            class="btn preset-filled-error-500"
             >
             <IconTrash />
           </button>
         </div>
       {/each}
-      </section>
-    {/if}
-  </div>
-  <div class="relative col-span-full">
-    <input
-      type="text"
-      oninput={handleInput}
-      onfocus={() => showDropdown = true}
-    placeholder="Search songs using MusicBrainz Lucene syntax..."
-    class="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-primary-400 focus:border-transparent outline-none"
-    >
-    {#if showDropdown && searchResults.length > 0}
+    </section>
+  {/if}
+</div>
+
+<div class="mt-7 relative">
+  <input
+    type="text"
+    oninput={handleInput}
+    onfocus={() => showDropdown = true}
+    placeholder="Search and add songs using MusicBrainz Lucene syntax..."
+    class="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-primary-400 focus:border-transparent"
+  >
+  {#if showDropdown && searchResults.length > 0}
+    <OutClick onOutClick={() => showDropdown = false}>
       <div class="absolute w-full bg-white dark:bg-gray-800 mt-1 rounded-lg shadow-lg border border-gray-200 dark:border-gray-700 max-h-60 overflow-y-auto z-10">
         {#each searchResults as song}
           <div
             class="p-3 hover:bg-primary-100 dark:hover:bg-primary-700 cursor-pointer transition-colors duration-150"
             onclick={async () => await addItem(song)}
             >
-            <div class="font-medium text-gray-900 dark:text-gray-100">{song.title}</div>
+            <div class="font-medium text-gray-900 dark:text-gray-100">{song.title} <span class="text-sm text-gray-500"><SongDuration song={song} /></span></div>
             <div class="text-sm text-gray-500 dark:text-gray-200">{song.artist.title}</div>
             <div class="text-sm text-gray-500 dark:text-gray-200">{song.release.title} ({song.release.date})</div>
           </div>
         {/each}
       </div>
-    {/if}
-  </div>
+    </OutClick>
+  {/if}
 </div>
