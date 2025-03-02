f34a523c69ac6d1410a8f08bd52ff1752403059c
Abhinav Tushar <abhinav@lepisma.xyz>
2025-03-02 14:25:13 +0530

    Show data policy only when url is mbzlists

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,10 @@
 <script>
   import { Modal } from '@skeletonlabs/skeleton-svelte';
+  import { env } from '$env/dynamic/public';
 
   let aboutModalState = $state(false);
   let dataModalState = $state(false);
+  let showDataPolicySection = env.PUBLIC_BASE_URL ? env.PUBLIC_BASE_URL === 'https://mbzlists.com/' : false;
 
   function modalClose() {
     aboutModalState = false;
@@ -38,36 +40,38 @@
       {/snippet}
     </Modal>
 
-    <Modal
-      bind:open={dataModalState}
-      triggerBase="anchor"
-      contentBase="card p-4 bg-surface-100 dark:bg-surface-900 space-y-4 shadow-xl max-w-screen-sm rounded-lg"
-      backdropClasses="backdrop-blur-sm"
-      >
-      {#snippet trigger()}Data Policy{/snippet}
-      {#snippet content()}
-      <header class="flex justify-between">
-        <h4 class="h4">Data Policy</h4>
-      </header>
-      <article class="space-y-3">
-        <p>
-          Since there is no login, there are no personally identified
-          information stored in this tool. As you create lists, your
-          device <i>remembers</i> them by storing in its local storage. If you
-          change devices or don't remember edit or view link of a list, you will
-          loose them.
-        </p>
-        <p>
-          Since the data around which songs are grouped together in lists could
-          be important for studying patterns and building recommendations,
-          mbzlists.com will periodically release, name-redacted, database dump
-          under a non-commercial license.
-        </p>
-        <p>
-          Additionally, you can self-host <i>mbzlists</i>. Check out <a class="anchor" href="https://github.com/lepisma/mbzlists">the source</a> for that.
-        </p>
-      </article>
-      {/snippet}
-    </Modal>
+    {#if showDataPolicySection}
+      <Modal
+        bind:open={dataModalState}
+        triggerBase="anchor"
+        contentBase="card p-4 bg-surface-100 dark:bg-surface-900 space-y-4 shadow-xl max-w-screen-sm rounded-lg"
+        backdropClasses="backdrop-blur-sm"
+        >
+        {#snippet trigger()}Data Policy{/snippet}
+        {#snippet content()}
+        <header class="flex justify-between">
+          <h4 class="h4">Data Policy</h4>
+        </header>
+        <article class="space-y-3">
+          <p>
+            Since there is no login, there are no personally identified
+            information stored in this tool. As you create lists, your
+            device <i>remembers</i> them by storing in its local storage. If you
+            change devices or don't remember edit or view link of a list, you will
+            loose them.
+          </p>
+          <p>
+            Since the data around which songs are grouped together in lists could
+            be important for studying patterns and building recommendations,
+            mbzlists.com will periodically release, name-redacted, database dump
+            under a non-commercial license.
+          </p>
+          <p>
+            Additionally, you can self-host <i>mbzlists</i>. Check out <a class="anchor" href="https://github.com/lepisma/mbzlists">the source</a> for that.
+          </p>
+        </article>
+        {/snippet}
+      </Modal>
+    {/if}
   </nav>
 </header>
