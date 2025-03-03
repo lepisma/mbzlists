67aa8a7941fd37316816ee9e34f803870fd43c59
Abhinav Tushar <abhinav@lepisma.xyz>
2025-03-03 17:33:38 +0530

    Fix song playback fns in editorjs component

--- src/lib/components/PlayListEditor.svelte
+++ src/lib/components/PlayListEditor.svelte
@@ -1,16 +1,25 @@
 <script lang='ts'>
   import { onMount, getContext } from 'svelte';
   import { browser } from '$app/environment';
-  import type { Song, List } from '$lib/types';
-  import { getCoverArt } from '$lib/mb';
+  import type { Song } from '$lib/types';
   import { lengthToDuration } from '$lib/utils';
   import type { ToastContext } from '@skeletonlabs/skeleton-svelte';
+import { playTrackOnYt, playTrackOnSpotify } from '$lib/playback';
 
   let toast: ToastContext = $state(getContext('toast'));
   let { list, isEdit = false } = $props();
 
   if (browser) {
-    window.test = function test() { toast.create({ description: 'Started Spotify search!' }); }
+    // HACK: We put most of the items on the global namespace for now
+    window.list = list;
+    window.playMBIDOnYt = function playMBIDOnYt(mbid: string) {
+      let song = list.items.find(it => it.mbid == mbid);
+      playTrackOnYt(song, toast);
+    }
+    window.playMBIDOnSpotify = function playMBIDOnSpotify(mbid: string) {
+      let song = list.items.find(it => it.mbid == mbid);
+      playTrackOnSpotify(song, toast);
+    }
   }
 
   const styleText = `
@@ -62,8 +71,8 @@
           <a class='anchor' href='https://musicbrainz.org/release/${song.release.mbid}'>${song.release.title} (${song.release.date})</a>
         </div>
         <div class='mt-1 flex space-x-2'>
-          <button title='Play on Youtube' onclick='test()'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 256 180"><path fill="#f00" d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134"/><path fill="#fff" d="m102.421 128.06l66.328-38.418l-66.328-38.418z"/></svg></button>
-          <button title='Play on Spotify' onclick='test()'><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 256 256"><path fill="#1ed760" d="M128 0C57.308 0 0 57.309 0 128c0 70.696 57.309 128 128 128c70.697 0 128-57.304 128-128C256 57.314 198.697.007 127.998.007zm58.699 184.614c-2.293 3.76-7.215 4.952-10.975 2.644c-30.053-18.357-67.885-22.515-112.44-12.335a7.98 7.98 0 0 1-9.552-6.007a7.97 7.97 0 0 1 6-9.553c48.76-11.14 90.583-6.344 124.323 14.276c3.76 2.308 4.952 7.215 2.644 10.975m15.667-34.853c-2.89 4.695-9.034 6.178-13.726 3.289c-34.406-21.148-86.853-27.273-127.548-14.92c-5.278 1.594-10.852-1.38-12.454-6.649c-1.59-5.278 1.386-10.842 6.655-12.446c46.485-14.106 104.275-7.273 143.787 17.007c4.692 2.89 6.175 9.034 3.286 13.72zm1.345-36.293C162.457 88.964 94.394 86.71 55.007 98.666c-6.325 1.918-13.014-1.653-14.93-7.978c-1.917-6.328 1.65-13.012 7.98-14.935C93.27 62.027 168.434 64.68 215.929 92.876c5.702 3.376 7.566 10.724 4.188 16.405c-3.362 5.69-10.73 7.565-16.4 4.187z"/></svg></button>
+          <button title='Play on Youtube' onclick='playMBIDOnYt(\"${song.mbid}\")'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 256 180"><path fill="#f00" d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134"/><path fill="#fff" d="m102.421 128.06l66.328-38.418l-66.328-38.418z"/></svg></button>
+          <button title='Play on Spotify' onclick='playMBIDOnSpotify(\"${song.mbid}\")'><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 256 256"><path fill="#1ed760" d="M128 0C57.308 0 0 57.309 0 128c0 70.696 57.309 128 128 128c70.697 0 128-57.304 128-128C256 57.314 198.697.007 127.998.007zm58.699 184.614c-2.293 3.76-7.215 4.952-10.975 2.644c-30.053-18.357-67.885-22.515-112.44-12.335a7.98 7.98 0 0 1-9.552-6.007a7.97 7.97 0 0 1 6-9.553c48.76-11.14 90.583-6.344 124.323 14.276c3.76 2.308 4.952 7.215 2.644 10.975m15.667-34.853c-2.89 4.695-9.034 6.178-13.726 3.289c-34.406-21.148-86.853-27.273-127.548-14.92c-5.278 1.594-10.852-1.38-12.454-6.649c-1.59-5.278 1.386-10.842 6.655-12.446c46.485-14.106 104.275-7.273 143.787 17.007c4.692 2.89 6.175 9.034 3.286 13.72zm1.345-36.293C162.457 88.964 94.394 86.71 55.007 98.666c-6.325 1.918-13.014-1.653-14.93-7.978c-1.917-6.328 1.65-13.012 7.98-14.935C93.27 62.027 168.434 64.68 215.929 92.876c5.702 3.376 7.566 10.724 4.188 16.405c-3.362 5.69-10.73 7.565-16.4 4.187z"/></svg></button>
         </div>
     `;
 
--- src/routes/edit/[editid]/+page.svelte
+++ src/routes/edit/[editid]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang='ts'>
   import { page } from '$app/stores';
-  import { onMount, getContext } from 'svelte';
+  import { onMount } from 'svelte';
   import debounce from 'lodash/debounce';
   import SongDuration from '$lib/components/SongDuration.svelte';
   import PlayListDuration from '$lib/components/PlayListDuration.svelte';
@@ -9,21 +9,11 @@
   import PlayListEditor from '$lib/components/PlayListEditor.svelte';
   import ShareButton from '$lib/components/ShareButton.svelte';
   import { loadEditableList, saveList } from '$lib/ops';
-  import { getCoverArt, queryMB } from '$lib/mb';
+  import { queryMB } from '$lib/mb';
   import type { EditableList } from '$lib/types';
-  import { playTrackOnSpotify, playTrackOnYt } from '$lib/playback';
   import { rememberItem } from '$lib/utils';
   import { formatDistanceToNow } from 'date-fns';
-  import IconTrash from 'virtual:icons/la/trash';
-  import IconYoutubeIcon from 'virtual:icons/logos/youtube-icon';
-  import IconSpotify from 'virtual:icons/logos/spotify';
-  import IconDrag from 'virtual:icons/clarity/drag-handle-line';
-  import { overrideItemIdKeyNameBeforeInitialisingDndZones, dndzone } from 'svelte-dnd-action';
-  import { flip } from 'svelte/animate';
   import { OutClick } from 'svelte-outclick';
-  import type { ToastContext } from '@skeletonlabs/skeleton-svelte';
-
-  overrideItemIdKeyNameBeforeInitialisingDndZones('mbid');
 
   let list: EditableList = $state({
      viewId: '',
@@ -36,7 +26,6 @@
      description: '',
   });
 
-  let toast: ToastContext = $state(getContext('toast'));
   let searchQuery = $state('');
   let searchResults = $state([]);
   let showDropdown = $state(false);
@@ -47,12 +36,6 @@
     await saveList(list);
     showDropdown = false;
     searchQuery = '';
-  }
-
-  async function removeItem(mbid) {
-    list.items = list.items.filter(it => it.mbid !== mbid);
-    list = {...list, lastModifiedOn: new Date()};
-    await saveList(list);
   }
 
   async function searchSongs(query) {
@@ -75,17 +58,6 @@
     list = {...list, lastModifiedOn: new Date()};
     await saveList(list);
   }, 500);
-
-  const handleDescriptionEdits = debounce(async () => {
-    list = {...list, lastModifiedOn: new Date()};
-    await saveList(list);
-  }, 500);
-
-  async function handleSort(e) {
-    list.items = e.detail.items;
-    list = {...list, lastModifiedOn: new Date()};
-    await saveList(list);
-  }
 
   onMount(async () => {
     list = await loadEditableList(list.editId);
@@ -128,44 +100,6 @@
     <p class="italic">Your list is empty. Search for songs to add!</p>
   {:else}
     <PlayListEditor list={list} isEdit={true}/>
-    <section use:dndzone={{ items: list.items, flipDurationMs: 200 }} onconsider={handleSort} onfinalize={handleSort}>
-      {#each list.items as item(item.mbid)}
-        <div class="flex items-center justify-between mt-2 p-3 pl-1 rounded-lg border shadow-md" animate:flip={{duration: 200}}>
-          <div class="flex items-center">
-            <div class="mr-1"><IconDrag width="36" height="36" style="color: gray" /></div>
-            <div class="flex">
-              <div>
-                {#await getCoverArt(item.release.mbid)}
-                  <img src="" alt="" class="w-16 h-16 mr-2 object-cover placeholder animate-pulse rounded-md" />
-                {:then value}
-                  <img src={value || "https://placehold.co/100?text=NA"} alt="" class="w-16 h-16 mr-2 object-cover rounded-md" />
-                {:catch error}
-                  <img src="https://placehold.co/100?text=NA" alt="" class="w-16 h-16 mr-2 object-cover placeholder rounded-md" />
-                {/await}
-              </div>
-              <div>
-                <div class="font-medium"><a class="anchor" href={`https://musicbrainz.org/recording/${item.mbid}`}>{item.title}</a>
-                  <span class="text-sm text-gray-500"><SongDuration song={item} /></span>
-                </div>
-                <div class="text-sm"><a class="anchor" href={`https://musicbrainz.org/artist/${item.artist.mbid}`}>{item.artist.title}</a></div>
-                <div class="text-sm text-gray-500"><a class="anchor" href={`https://musicbrainz.org/release/${item.release.mbid}`}>{item.release.title} ({item.release.date})</a></div>
-                <div class="mt-2 flex space-x-2">
-                  <button title="Play on Youtube" onclick={async () => await playTrackOnYt(item, toast) }><IconYoutubeIcon /></button>
-                  <button title="Play on Spotify" onclick={async () => await playTrackOnSpotify(item, toast)}><IconSpotify /></button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <button
-            type="button"
-            onclick={async () => await removeItem(item.mbid)}
-            class="btn preset-filled-error-500"
-            >
-            <IconTrash />
-          </button>
-        </div>
-      {/each}
-    </section>
   {/if}
 </div>
 
