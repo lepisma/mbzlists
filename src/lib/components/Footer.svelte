865cc6ee73e4f6c68e926989f001fb21c4026ff5
Abhinav Tushar <abhinav@lepisma.xyz>
2025-03-11 16:29:54 +0530

    Move content from header to footer and add more placeholders

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,18 @@
 <script>
+  import { Modal } from '@skeletonlabs/skeleton-svelte';
+  import { env } from '$env/dynamic/public';
+  import IconDoc from 'virtual:icons/solar/document-add-linear';
+  import IconDB from 'virtual:icons/solar/database-outline';
+  import IconBug from 'virtual:icons/solar/bug-linear';
+  import IconContact from 'virtual:icons/solar/chat-square-call-linear';
+
+  let dataModalState = $state(false);
+  let showDataPolicySection = env.PUBLIC_BASE_URL ? env.PUBLIC_BASE_URL === 'https://mbzlists.com/' : false;
+
+  function modalClose() {
+    dataModalState = false;
+  }
+
   function pickRandomQuote() {
     let quotes = [
       '"Hello" - Adele',
@@ -10,11 +24,53 @@
   }
 </script>
 
-<footer class="p-6 rounded-b-lg">
-  <blockquote class="blockquote mb-4">
-    {pickRandomQuote()}
-  </blockquote>
-  <p class="text-italic">
-    <em>mbzlists</em> is an <a class="anchor" href="https://github.com/lepisma/mbzlists">Open Source Software</a> designed and made in Bengaluru by <a class="anchor" href="https://unnu.so">unnu</a> and <a class="anchor" href="https://lepisma.xyz">lepisma</a>
-  </p>
+<footer class="p-6 rounded-b-lg grid grid-cols-2">
+  <div>
+    <ul>
+      <li><a href="#" class="anchor flex items-center disabled"><IconDoc class="mr-2" /> Documentation</a></li>
+      {#if showDataPolicySection}
+        <Modal
+          bind:open={dataModalState}
+          triggerBase="anchor flex items-center"
+          contentBase="card p-4 bg-surface-100 dark:bg-surface-900 space-y-4 shadow-xl max-w-screen-sm rounded-lg"
+          backdropClasses="backdrop-blur-sm"
+          >
+          {#snippet trigger()}<IconDB class="mr-2" /> Data Policy{/snippet}
+          {#snippet content()}
+          <header class="flex justify-between">
+            <h4 class="h4">Data Policy</h4>
+          </header>
+          <article class="space-y-3">
+            <p>
+              There are no personally identified information stored here. As you
+              create lists, your device <i>remembers</i> them by storing in its
+              local storage. If you change devices or don't remember edit or view
+              link of a list, you will loose them.
+            </p>
+            <p>
+              On mbzlists.com, public lists will be downloadable under a
+              non-commercial license. Your own private lists can be downloaded in
+              a dump. These export features are yet to be added.
+            </p>
+            <p>
+              You can self-host <i>mbzlists</i> to get even more control on your
+              data. Check out <a class="anchor" href="https://github.com/lepisma/mbzlists">the source</a> for that.
+            </p>
+          </article>
+          {/snippet}
+        </Modal>
+      {/if}
+      <li><a href="https://github.com/lepisma/mbzlists/issues" target="_blank" class="anchor flex items-center"><IconBug class="mr-2" /> Issues & Feature Requests</a></li>
+      <li><a href="#" class="anchor flex items-center disabled"><IconContact class="mr-2" /> Support & Contact</a></li>
+    </ul>
+  </div>
+  <div>
+    <blockquote class="blockquote">
+      {pickRandomQuote()}
+    </blockquote>
+
+    <div class="mt-4">
+      <em>mbzlists</em> is an <a class="anchor" href="https://github.com/lepisma/mbzlists">Open Source Software</a> designed and made in Bengaluru by <a class="anchor" href="https://unnu.so">unnu</a> and <a class="anchor" href="https://lepisma.xyz">lepisma</a>.
+    </div>
+  </div>
 </footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,10 @@
 <script>
   import { Modal } from '@skeletonlabs/skeleton-svelte';
-  import { env } from '$env/dynamic/public';
 
   let aboutModalState = $state(false);
-  let dataModalState = $state(false);
-  let showDataPolicySection = env.PUBLIC_BASE_URL ? env.PUBLIC_BASE_URL === 'https://mbzlists.com/' : false;
 
   function modalClose() {
     aboutModalState = false;
-    dataModalState = false;
   }
 </script>
 
@@ -38,39 +34,5 @@
       </article>
       {/snippet}
     </Modal>
-
-    {#if showDataPolicySection}
-      <Modal
-        bind:open={dataModalState}
-        triggerBase="anchor"
-        contentBase="card p-4 bg-surface-100 dark:bg-surface-900 space-y-4 shadow-xl max-w-screen-sm rounded-lg"
-        backdropClasses="backdrop-blur-sm"
-        >
-        {#snippet trigger()}Data Policy{/snippet}
-        {#snippet content()}
-        <header class="flex justify-between">
-          <h4 class="h4">Data Policy</h4>
-        </header>
-        <article class="space-y-3">
-          <p>
-            There are no personally identified information stored here. As you
-            create lists, your device <i>remembers</i> them by storing in its
-            local storage. If you change devices or don't remember edit or view
-            link of a list, you will loose them.
-          </p>
-          <p>
-            On mbzlists.com, public lists will be downloadable under a
-            non-commercial license. Your own private lists can be downloaded in
-            a dump. These export features are yet to be added.
-          </p>
-          <p>
-            You can self-host <i>mbzlists</i> to get even more control on your
-            data. Check out <a class="anchor"
-            href="https://github.com/lepisma/mbzlists">the source</a> for that.
-          </p>
-        </article>
-        {/snippet}
-      </Modal>
-    {/if}
   </nav>
 </header>
