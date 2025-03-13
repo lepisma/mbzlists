532f2e7f0f2dad563b744ac3ec4819cedbd3ed8f
Abhinav Tushar <abhinav@lepisma.xyz>
2025-03-13 15:39:51 +0530

    Allow dismax searches for better user experience

--- src/lib/mb.ts
+++ src/lib/mb.ts
@@ -1,6 +1,11 @@
 import type { Song } from '$lib/types';
 
-async function checkImage(url: string): boolean {
+interface SongMatch {
+  score: number;
+  song: Song;
+}
+
+async function checkImage(url: string): Promise<boolean> {
   try {
     const response = await fetch(url, { method: "HEAD" });
     return response.ok;
@@ -17,25 +22,26 @@ export async function getCoverArt(releaseId: string) {
   };
 }
 
-export async function queryMB(query: string): Promise<Song[]> {
-  const url = `https://musicbrainz.org/ws/2/recording?query=${encodeURIComponent(query)}&fmt=json`;
-
+async function fetchSearchResults(url: string): Promise<SongMatch[]> {
   try {
     const res = await fetch(url);
     const data = await res.json();
-    let searchResults: Song[] = data.recordings?.map(rec => ({
-      title: rec.title,
-      artist: {
-        title: rec['artist-credit']?.[0]?.name,
-        mbid: rec['artist-credit']?.[0]?.artist.id
-      },
-      mbid: rec.id,
-      release: {
-        date: rec['releases']?.[0]?.date,
-        mbid: rec['releases']?.[0]?.id,
-        title: rec['releases']?.[0]?.title
-      },
-      length: rec.length
+    let searchResults: SongMatch[] = data.recordings?.map((rec: any) => ({
+      score: rec.score,
+      song: {
+        title: rec.title,
+        artist: {
+          title: rec['artist-credit']?.[0]?.name,
+          mbid: rec['artist-credit']?.[0]?.artist.id
+        },
+        mbid: rec.id,
+        release: {
+          date: rec['releases']?.[0]?.date,
+          mbid: rec['releases']?.[0]?.id,
+          title: rec['releases']?.[0]?.title
+        },
+        length: rec.length
+      }
     })) || [];
 
     return searchResults;
@@ -43,4 +49,22 @@ export async function queryMB(query: string): Promise<Song[]> {
     console.error('Error fetching songs:', error);
     return [];
   }
+}
+
+function isQueryAdvanced(query: string): boolean {
+  return query.includes('artist:') || query.includes('release:');
+}
+
+export async function queryMB(query: string): Promise<Song[]> {
+  const n = 25;
+
+  let url;
+  if (isQueryAdvanced(query)) {
+    url = `https://musicbrainz.org/ws/2/recording?query=${encodeURIComponent(query)}&fmt=json&limit=${n}`;
+  } else {
+    url = `https://musicbrainz.org/ws/2/recording?query=${encodeURIComponent(query)}&fmt=json&limit=${n}&dismax=true`;
+  }
+
+  const matches = await fetchSearchResults(url);
+  return matches.map(m => m.song);
 }
